07da8c38d8701df0e5b53ccb54119a2c0cc05975
li.qian <li.qian@immomo.com>
2020-07-02 20:13:26 +0800

    dlna demo

---
+++ app/src/main/java/com/stephenone/upnpdemo/MainActivity.java
@@ -0,0 +1,19 @@
+//package com.stephenone.upnpdemo;
+//
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//import org.fourthline.cling.demo.android.browser.BrowserActivity;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Intent intent = new Intent();
+//        intent.setClass(this, BrowserActivity.class);
+//        startActivity(intent);
+//    }
+//}
